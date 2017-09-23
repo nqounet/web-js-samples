27bc8e24866b4d1ff05bb7a3f8b0cfac9ae57013
nqounet <mail@nqou.net>
2017-09-23 20:25:55 +0900

    update(master)

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,8 @@ module.exports = function (grunt) {
                     src: [
                         '.tmp',
                         '<%= config.dist %>/*',
-                        '!<%= config.dist %>/.git*'
+                        '!<%= config.dist %>/.git*',
+                        '!<%= config.dist %>/CNAME'
                     ]
                 }]
             },
